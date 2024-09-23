8f447ce2ba996ffb500b95f8e2c64131ecc2b23f
MCProtein <chss3339@gmail.com>
2024-09-24 00:47:01 +0900

    mockClear, mockReset, mockRestore

--- src/mockFunction.spec.ts
+++ src/mockFunction.spec.ts
@@ -5,11 +5,15 @@ afterEach(() => {
 })
 
 test('obj.minus 함수가 1번 호출되었다. (spy 삽입)', () => {
-  jest.spyOn(obj, 'minus')
+  const spyFn = jest.spyOn(obj, 'minus')
   const result = obj.minus(1, 2)
 
   expect(obj.minus).toHaveBeenCalledTimes(1)
   expect(result).toBe(-1)
+
+  spyFn.mockClear() // Times, With 초기화
+  spyFn.mockReset() // mockClear + mockImplementation(() => {})
+  spyFn.mockRestore() // 아예 전부 없애버림. obj.minus가 원래대로 돌아감.
 })
 
 test('obj.minus에 스파이를 심고 실행도 안되게.', () => {
