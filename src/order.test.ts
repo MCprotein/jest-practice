c5166b2b083835e67f9f3aeace4606db5e3a52eb
yoonsooshin <yoonsoo@jentestore.com>
2024-10-15 13:49:37 +0900

    mock 객체의 활용

--- src/order.test.ts
+++ src/order.test.ts
@@ -15,7 +15,7 @@ test('first -> second -> third - 함수 실행 순서 테스트', () => {
   expect(spy3.mock.invocationCallOrder[0]).toBeGreaterThan(spy2.mock.invocationCallOrder[0])
 })
 
-test('first -> second -> third 2 - 함수 실행 순서 테스트', () => {
+test.skip('first -> second -> third 2 - 함수 실행 순서 테스트', () => {
   const spy1 = jest.fn(first)
   const spy2 = jest.fn(second)
   const spy3 = jest.fn(third)
@@ -28,4 +28,20 @@ test('first -> second -> third 2 - 함수 실행 순서 테스트', () => {
   console.log(spy3.mock.invocationCallOrder[0])
   expect(spy1).toHaveBeenCalledBefore(spy2)
   expect(spy3.mock.invocationCallOrder[0]).toBeGreaterThan(spy2.mock.invocationCallOrder[0])
+})
+
+test('인수의 일부 테스트', () => {
+  const fn = jest.fn()
+  fn({
+    a: {
+      b: {
+        c: 'hello'
+      },
+      d: 'bye'
+    },
+    e: ['f']
+  })
+  
+  // 첫 번째로 호출된 첫 번째 인자의 a.b.c가 hello인지 테스트
+  expect(fn.mock.calls[0][0].a.b.c).toBe('hello')
 })
