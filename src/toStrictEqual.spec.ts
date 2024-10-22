e1bc42f0f585c902bfb5888f2eadc274510b5330
MCProtein <chss3339@gmail.com>
2024-10-22 23:15:09 +0900

    유사한 값도 통과시키기 - expect.any, expect.closeTo

--- src/toStrictEqual.spec.ts
+++ src/toStrictEqual.spec.ts
@@ -9,3 +9,7 @@ test('배열끼리도 toStrictEqual 써야한다.', () => {
   expect([1, 2, 3]).toStrictEqual([1, 2, 3])
   expect([1, 2, 3]).not.toBe([1, 2, 3])
 })
+
+test('아무거나 인정', () => {
+  expect(obj()).toStrictEqual({ a: expect.any(String) })
+})
